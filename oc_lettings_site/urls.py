d004fc64b8ab4d2a92e6accaa4e9c42289caac35
andre <andresuruguepro@gmail.com>
2025-07-23 11:07:47 +0200

    mise en place des routages urls dans letting et profile et reroutage dans oc_letting

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,3 +1,4 @@
+""" OLD VERSION
 from django.contrib import admin
 from django.urls import path
 
@@ -9,5 +10,16 @@ urlpatterns = [
     path('lettings/<int:letting_id>/', views.letting, name='letting'),
     path('profiles/', views.profiles_index, name='profiles_index'),
     path('profiles/<str:username>/', views.profile, name='profile'),
+    path('admin/', admin.site.urls),
+]
+"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('lettings/', include('lettings.urls')),
+    path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
 ]
